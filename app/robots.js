b3140b1de35a5d1e3ac0adc02ee460657acc94a7
futureurhero-stack <futureurhero@gmail.com>
2025-12-21 20:12:57 +0900

    Add: robots.txt 추가

---
+++ app/robots.js
@@ -0,0 +1,15 @@
+export default function robots() {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://freeimage-tools-next.vercel.app';
+  
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
+
