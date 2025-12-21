22b8e24199c803f943107de3d2c3054a95b5b74f
futureurhero-stack <futureurhero@gmail.com>
2025-12-21 20:47:31 +0900

    Add: robots.txt 추가, 구글 애드센스 메타 태그 추가

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,18 @@ import "./globals.css";
 
 export const metadata = {
   title: "FreeImageTools – JPG Converter & Image Resizer",
-  description: "Free JPG converter & image size reducer in your browser."
+  description: "Free JPG converter & image size reducer in your browser.",
+  other: {
+    "google-adsense-account": "ca-pub-2947007400947272",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-2947007400947272" />
+      </head>
       <body>{children}</body>
     </html>
   );
